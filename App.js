02cb2607c41cc75b3ef6ba5ccaffc3253fb38472
eshkolnik <thehawkeye453@gmail.com>
2021-06-16 16:18:20 +0300

    Update App.js

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <Text>Hello</Text>
+      <Text>Hello namja</Text>
       <StatusBar style="auto" />
     </View>
   );
